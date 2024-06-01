3ad6df61877e6b80d6271f9858fcd82d7ea68a40
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2024-06-01 23:10:02 +0530

    added contact code structure and contactrequst strutre

--- BiteSpeed_Assignments/src/main/java/com/example/BiteSpeed_Assignments/Model/Contact.java
+++ BiteSpeed_Assignments/src/main/java/com/example/BiteSpeed_Assignments/Model/Contact.java
@@ -1,4 +1,40 @@
 package com.example.BiteSpeed_Assignments.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String phone_number;
+
+    private String email_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "linked_id")
+    private Contact linked_id;
+
+    private String link_precedence;
+
+    @CreationTimestamp
+    private Date created_at;
+
+    @UpdateTimestamp
+    private Date updated_at;
+
+    private Date deleted_at;
 }
--- BiteSpeed_Assignments/src/main/java/com/example/BiteSpeed_Assignments/Model/ContactRequest.java
+++ BiteSpeed_Assignments/src/main/java/com/example/BiteSpeed_Assignments/Model/ContactRequest.java
@@ -1,5 +1,14 @@
 package com.example.BiteSpeed_Assignments.Model;
 
-public class ContactRequest {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactRequest {
+    private String email;
+
+    private String phoneNumber;
 }
