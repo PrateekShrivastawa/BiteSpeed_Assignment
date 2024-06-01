7a387af2e467296dfd0edf0aa58fc53aa6ce2546
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2024-06-01 22:48:47 +0530

    added spring initializer code which contain starting boiler plate code and dependency

---
+++ BiteSpeed_Assignments/src/test/java/com/example/BiteSpeed_Assignments/BiteSpeedAssignmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BiteSpeed_Assignments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteSpeedAssignmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
